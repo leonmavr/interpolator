bfb08fbe2c048bd6611472094837ae942b73cf47
Leontios Mavropalias <0xleo.git@gmail.com>
2022-01-14 17:46:05 +0100

    driver code: interpolate off the boundaries too

--- main.cpp
+++ main.cpp
@@ -7,22 +7,21 @@
 
 
 int main() {
-
 	// write results to file to visualise them later
 	// it shall contain:
 	// anchor points as (x, y), newline, interpolated points (x, y)
 	std::ofstream file_out;
 	file_out.open("out.txt");
 
-    auto interp = std::make_unique<Interpolator>("smoothstep");
+    auto interp = std::make_unique<Interpolator>("linear");
 	std::vector<std::pair<double, double>> anchors;
-	anchors.push_back(std::make_pair(12.0, 16.0));
-	anchors.push_back(std::make_pair(-1.0, -10.0));
-	anchors.push_back(std::make_pair(1.0, -5.0));
-	anchors.push_back(std::make_pair(18.0, 2.0));
-	anchors.push_back(std::make_pair(10, 10.0));
-	anchors.push_back(std::make_pair(20.0, 1.5));
-	anchors.push_back(std::make_pair(5.5, 8.0));
+	anchors.push_back(std::make_pair(12.0, 8.0));
+	anchors.push_back(std::make_pair(-1.0, -5.0));
+	anchors.push_back(std::make_pair(1.0, -2.5));
+	anchors.push_back(std::make_pair(18.0, 1.0));
+	anchors.push_back(std::make_pair(10, 5.0));
+	anchors.push_back(std::make_pair(20.0, 0.75));
+	anchors.push_back(std::make_pair(5.5, 4.0));
 
 	for (const auto& a: anchors) {
 		interp->insert(a.first, a.second);
@@ -31,7 +30,7 @@ int main() {
 	file_out << std::endl;
 	
 	double dx = 0.01;
-	for (double x = -1.0; x < 20.0; x += dx)
+	for (double x = -3.0; x < 22.0; x += dx)
 		file_out << x << ", " << interp->interpolate(x) << std::endl;
 
 	file_out.close();
